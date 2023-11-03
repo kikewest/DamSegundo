197ad601d1f02c8c743a4b1786fffed658e0ffbf
kikewest <ebellidoperejil2@gmail.com>
2023-11-03 17:41:05 +0100

    seguir en casa

--- mapas/src/mapas/Map1.java
+++ mapas/src/mapas/Map1.java
@@ -1,13 +1,19 @@
 package mapas;
 
 import java.awt.EventQueue;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Map1 {
 
 	private JFrame frame;
-
+	private Map<String, Double> notas = new HashMap<>();
 	/**
 	 * Launch the application.
 	 */
@@ -36,8 +42,28 @@ public class Map1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 619, 435);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnAniadir = new JButton("Añadir");
+		btnAniadir.setBounds(10, 339, 101, 46);
+		frame.getContentPane().add(btnAniadir);
+		
+		JButton btnVer = new JButton("Ver");
+		btnVer.setBounds(121, 339, 101, 46);
+		frame.getContentPane().add(btnVer);
+		
+		JButton btnActualizar = new JButton("Actualizar");
+		btnActualizar.setBounds(232, 339, 101, 46);
+		frame.getContentPane().add(btnActualizar);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.setBounds(343, 339, 101, 46);
+		frame.getContentPane().add(btnEliminar);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.setBounds(454, 339, 101, 46);
+		frame.getContentPane().add(btnSalir);
 	}
-
 }
